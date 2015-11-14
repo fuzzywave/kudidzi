ad3f0d1ddf2be46aba1be2c538941ea97eceed6c
Yigiter <yigiteryigiter@gmail.com>
2015-11-14 21:21:59 +0200

    initial commit

---
+++ desktop/src/com/fuzzywave/kududzi/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.fuzzywave.kududzi.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.fuzzywave.kududzi.KududziGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new KududziGame(), config);
+	}
+}
